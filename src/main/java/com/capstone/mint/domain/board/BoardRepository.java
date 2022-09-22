ae6855227dfb2667ba837a21dbca76cbaaa5515d
uijinpark <uj.park@icloud.com>
2022-09-22 12:15:27 +0900

    Add Board Dto, Board Api modify

--- src/main/java/com/capstone/mint/domain/board/BoardRepository.java
+++ src/main/java/com/capstone/mint/domain/board/BoardRepository.java
@@ -1,4 +1,4 @@
-package com.capstone.mint.entity;
+package com.capstone.mint.domain.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
