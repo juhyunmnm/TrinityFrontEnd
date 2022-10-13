81ed6d75e39bb7a7a24b05a6e01fdad10bdb33c3
uijinpark <uj.park@icloud.com>
2022-10-13 10:39:16 +0900

    feat : User entity/dto/service, Login API 생성
    스프링 시큐리티를 이용한 로그인 기능 일부 완성

--- src/main/java/com/capstone/mint/domain/BaseTimeEntity.java
+++ src/main/java/com/capstone/mint/domain/BaseTimeEntity.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
+    // Entity가 생성되어 저장될 때 시간이 자동 저장
     @CreatedDate
     private LocalDateTime createdDate;
 
