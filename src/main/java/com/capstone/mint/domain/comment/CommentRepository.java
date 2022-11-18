efa4e7dac3bf183b81b9d64eac7394509d698472
uijinpark <uj.park@icloud.com>
2022-11-18 17:49:48 +0900

    feat : 게시판, 댓글 기능 구현

---
+++ src/main/java/com/capstone/mint/domain/comment/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.capstone.mint.domain.comment;
+
+import com.capstone.mint.domain.post.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByPost(Post post);
+}
