03f8f4ffb3e48af748da75ee81e0814945843509
uijinpark <uj.park@icloud.com>
2022-11-29 23:12:49 +0900

    feat : TravisCi S3 연동

--- src/main/java/com/capstone/mint/domain/post/PostRepositoryImpl.java
+++ src/main/java/com/capstone/mint/domain/post/PostRepositoryImpl.java
@@ -1,10 +1,6 @@
 package com.capstone.mint.domain.post;
 
 import com.capstone.mint.web.dto.PageResponseDto;
-<<<<<<< HEAD
-=======
-import com.capstone.mint.domain.post.Post;
->>>>>>> origin/main
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,11 +10,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.stream.Collectors;
 
-<<<<<<< HEAD
 import static com.capstone.mint.domain.post.QPost.post;
-=======
-import static  com.capstone.mint.domain.post.QPost.post;
->>>>>>> origin/main
 
 @RequiredArgsConstructor
 public class PostRepositoryImpl implements PostRepositoryCustom{
